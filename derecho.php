c17e39b31f0f0d362ccc2aaefb0a3fca34c9a30d
HermiloMontoya <143041208+HermiloMontoya@users.noreply.github.com>
2024-02-27 23:04:59 -0600

    Mi sitio web Hermilo

---
+++ derecho.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarea</title>
+    <link rel="icon" href="https://sf.ezoiccdn.com/ezoimgfmt/www.vectorlogo.es/wp-content/uploads/2019/09/logo-vector-unach.jpg?ezimgfmt=rs:0x0/rscb1/ngcb1/notWebP" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+
+<body style="background-color: #000033;">
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+    <a class="navbar-brand" href="index.php">INICIO </a>
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="prueba.php">Contratos Informaticos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="prueba.php">Segundo Tema </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="prueba.php">Tercer Tema</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="prueba.php">Cuarto Tema</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="prueba.php">Quinto Tema</a>
+            </li>
+            
+            <form class="form-inline my-2 my-lg-0">
+            <div class="input-group">
+                <input class="form-control" type="search" placeholder="Buscar" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Buscar</button>
+            </div>
+        </form>
+    </div>
+</nav>
+<br><br>
+<div class="text-center text-white">
+  <h1>Repositorio digital Derecho Informatico</h1>
+  <h2>Facultad de contaduria y administracion</h2>
+</div><br><br>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div id="carouselExampleIndicators" class="carousel slide">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="controlador/controlUtilerias/imagenes/img1.jpg" class="d-block w-100" alt="..." width="auto" height="400px">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="controlador/controlUtilerias/imagenes/img2.jpg" class="d-block w-100" alt="..." width="auto" height="400px">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://blog.lemontech.com/wp-content/uploads/2020/03/derecho-informatico-1.jpg" class="d-block w-100" alt="..." width="auto" height="400px">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+        
+    </div>
+</div>
+
+
+
+<header>
+<div class="container text-center text-white">
+      <h1>Contratos Informáticos</h1> <br><br>
+    </div>
+    </header>
+    
+    <main>
+    <div class="container text-white">
+  <div class="row">
+    <div class="col">
+      <p class="text-center">Los contratos informáticos son acuerdos de voluntades entre partes para la prestación de servicios o comercialización de bienes relacionados con la informática. Tienen características propias y únicas, como ser atípicos, lo que significa que no están regulados específicamente por la ley pero pueden existir legalmente a través de la libertad de contratación. También, las partes involucradas suelen ser identificadas como proveedores de bienes y servicios informáticos y usuarios. <br><br>
+
+                            Determinar si un contrato informático es de naturaleza mercantil o civil puede ser complicado. Por ejemplo, si un estudiante contrata la paquetería básica de Microsoft Office, el proveedor (Microsoft Corporation) busca lucro, mientras que el estudiante simplemente necesita el software para uso personal. La decisión depende de si se considera el acto de comercio una especulación subjetiva o un intercambio de bienes o servicios con fines de lucro. Los tribunales han establecido criterios para determinar si un acto es de naturaleza comercial. <br><br>Estos criterios incluyen la intervención de un sujeto comerciante, la intermediación en el intercambio de bienes y la intención de obtener un lucro. Sin embargo, también se reconoce que la especulación comercial es subjetiva y depende de las intenciones individuales de las partes involucradas.</p>
+    </div>
+    <div class="col">
+      <p class="text-center">Es importante tener en cuenta que los contratos informáticos deben incluir cláusulas de propiedad intelectual (PI) para evitar riesgos para las partes involucradas. Estas cláusulas establecen la protección de los derechos de autor y otros derechos de propiedad intelectual sobre los bienes y servicios informáticos involucrados en el contrato. <br><br>
+
+      los contratos informáticos, en su mayoría, deben abordar cuestiones muy específicas, como cualquier otro contrato: objeto y consentimiento como elementos de existencia; capacidad, licitud del objeto, forma y ausencia de vicios como elementos de validez; y legitimación como elemento de eficacia (Sánchez Medal, 2008, p. 25).  <br><br>
+                        Además, los contratos informáticos deben cumplir con las características generales de los contratos, como el objeto y el consentimiento, la capacidad, la licitud del objeto, la forma y la ausencia de vicios, y la legitimación. <br><br>
+
+                        En resumen, los contratos informáticos son acuerdos de voluntades entre partes para la prestación de servicios o comercialización de bienes relacionados con la informática. Tienen características propias y únicas, como ser atípicos y requerir cláusulas de propiedad intelectual para evitar riesgos. Además, deben cumplir con las características generales de los contratos y pueden ser de naturaleza mercantil o civil, dependiendo de las intenciones de las partes involucradas.</p> <br>
+    </div>
+  </div>
+</div>
+</main>
+
+<footer class="footer text-white bg-dark">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        Hermilo Medardo Montoya Fuentes <br><br><br>
+      </div>
+    </div>
+  </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarea</title>
+    <link rel="icon" href="https://sf.ezoiccdn.com/ezoimgfmt/www.vectorlogo.es/wp-content/uploads/2019/09/logo-vector-unach.jpg?ezimgfmt=rs:0x0/rscb1/ngcb1/notWebP" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</head>
+
+
+<body style="background-color: #000033;">
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+    <a class="navbar-brand" href="#">INICIO </a>
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="prueba.php">Botón 1</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="prueba.php">Botón 2</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="prueba.php">Botón 3</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Botón 4
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="prueba.php">opcion1</a>
+                    <a class="dropdown-item" href="prueba.php">opcion 2</a>
+                    <a class="dropdown-item" href="prueba.php">opcion 3</a>
+                </div>
+            </li>
+            <form class="form-inline my-2 my-lg-0">
+            <div class="input-group">
+                <input class="form-control" type="search" placeholder="Buscar" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Buscar</button>
+            </div>
+        </form>
+    </div>
+</nav>
+<br><br>
+<div class="text-center text-white">
+  <h1>Repositorio digital Derecho Informatico</h1>
+  <h2>Facultad de contaduria y administracion</h2>
+</div><br><br>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div id="carouselExampleIndicators" class="carousel slide">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="controlador/controlUtilerias/imagenes/img1.jpg" class="d-block w-100" alt="..." width="auto" height="400px">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="controlador/controlUtilerias/imagenes/img2.jpg" class="d-block w-100" alt="..." width="auto" height="400px">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://blog.lemontech.com/wp-content/uploads/2020/03/derecho-informatico-1.jpg" class="d-block w-100" alt="..." width="auto" height="400px">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+        
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card bg-dark text-white">
+                <h5 class="card-header">
+                    Servicio Informáticos y <br> Entorno Laboral
+                </h5>
+                <div class="card-body">
+                <a href="derecho.php">
+                        <img src="controlador\controlUtilerias\imagenes\img1.svg" alt="Imagen SVG" style="width: 75%; height: auto;">
+                    </a>
+                </div>
+                <div class="card-footer">
+                Los servicios informáticos son un conjunto de soluciones tecnológicas y de asistencia especializada que abarcan el diseño, la implementación y el mantenimiento de sistemas informáticos para empresas y organizaciones. Estos servicios engloban una amplia gama de actividades, incluyendo el desarrollo de software personalizado, la gestión de redes y sistemas, la seguridad cibernética, la consultoría en tecnologías de la información y la infraestructura de soporte técnico.
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card bg-dark text-white">
+                <h5 class="card-header">
+                    Políticas Informáticas <br> Comercio Electrónico
+                </h5>
+                <div class="card-body">
+                <a href="derecho.php">
+                        <img src="controlador\controlUtilerias\imagenes\img2.svg" alt="Imagen SVG" style="width: 75%; height: auto;">
+                    </a>
+                </div>
+                <div class="card-footer">
+                    <p>La importancia que ha adquirido el comercio electrónico a nivel gubernamental se puede apreciar en la puesta en marcha de políticas orientadas a su potenciación y fomento, así como en la creación de organismos, instituciones oficiales o entidades publico/privadas cuyo objetivo es llevarlas a cabo. Así mismo también se puede apreciar en la creación de asociaciones con el mismo fin.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card bg-dark text-white">
+                <h5 class="card-header">
+                    Marco Legal Informático <br> Leyes Regulatorias en México
+                </h5>
+                <div class="card-body">
+                <a href="derecho.php">
+                        <img src="controlador\controlUtilerias\imagenes\img3.jpg" alt="Imagen SVG" style="width: 75%; height: auto;">
+                    </a>
+                </div>
+                <div class="card-footer">
+                El comercio internacional se ha desarrollado en dos niveles: en el contexto multilateral (en la OMC) y a nivel regional, con el establecimiento de los acuerdos de libre comercio y las uniones aduaneras. Por esta razón, en las décadas pasadas ha habido un cambio a nivel mundal, de un enfoque de negociaciones comerciales multilaterales a acuerdos regionales. Esto ha dado como resultado cerca de seiscientos tratados de libre comercio, uniones aduaneras y otros acuerdos 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<br><br>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</html>
